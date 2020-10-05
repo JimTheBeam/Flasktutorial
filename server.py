1e8463e69d4d5b1f6b648abcff0179399858018e
JimTheBeam <24dmitriy@mail.ru>
2020-10-06 01:35:35 +0300

    add weather

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 from flask import Flask
 
+from weather import weather_by_city
+
 app = Flask(__name__)
 
 
 
-@app.route('/')
-def hello():
-    return 'Hello World'
+@app.route("/")
+def index():
+    weather = weather_by_city("Moscow,Russia")
+    if weather:
+        return f"Температура: {weather['temp_C']}, ощущается: {weather['FeelsLikeC']}"
+    else:
+        return "Сервис погоды временно недоступен"
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
