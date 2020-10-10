a1b8d2ce1e2a4808121818a7eb2a819ed05cf5d5
JimTheBeam <24dmitriy@mail.ru>
2020-10-10 17:10:33 +0300

    flask migrations and user registation

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()],
@@ -12,3 +13,13 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
 
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()],
+                    render_kw={"class": "form-control"})
+    email = StringField('Email', validators=[DataRequired(), Email()],
+                    render_kw={"class": "form-control"})
+    password = PasswordField('Пароль', validators=[DataRequired()],
+                    render_kw={"class": "form-control"})
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')],
+                    render_kw={"class": "form-control"})
+    submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
